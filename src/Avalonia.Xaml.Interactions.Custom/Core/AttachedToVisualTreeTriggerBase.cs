a0891a5d3d8f005f0a205280c0e47adb5ffbbb53
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-04-04 11:28:46 +0200

    Create AttachedToVisualTreeTriggerBase.cs

---
+++ src/Avalonia.Xaml.Interactions.Custom/Core/AttachedToVisualTreeTriggerBase.cs
@@ -0,0 +1,37 @@
+using System;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions.Custom;
+
+/// <summary>
+/// A trigger that executes when the associated object is attached to the visual tree.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class AttachedToVisualTreeTriggerBase<T>  : DisposingTrigger<T> where T : Visual
+{
+    private IDisposable? _disposable;
+
+    /// <inheritdoc />
+    protected override IDisposable OnAttachedOverride()
+    {
+        return new DisposableAction(OnDelayedDispose);
+    }
+
+    /// <inheritdoc />
+    protected override void OnAttachedToVisualTree()
+    {
+        _disposable = OnAttachedToVisualTreeOverride();
+    }
+
+    /// <summary>
+    /// Called after the <see cref="StyledElementBehavior{T}.AssociatedObject"/> is attached to the visual tree.
+    /// </summary>
+    /// <returns>A disposable resource to be disposed when the behavior is detached.</returns>
+    protected abstract IDisposable OnAttachedToVisualTreeOverride();
+
+    private void OnDelayedDispose()
+    {
+        _disposable?.Dispose();
+        _disposable = null;
+    }
+}
