1263f908b6ecfa7673e3c724c112ba9122eef00d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-04-17 13:09:43 +0200

    Use ComparisonConditionTypeHelper.Compare(

--- src/Avalonia.Xaml.Interactions.Custom/Core/Conditional/IfElseActionBase.cs
+++ src/Avalonia.Xaml.Interactions.Custom/Core/Conditional/IfElseActionBase.cs
@@ -118,10 +118,12 @@ public abstract class IfElseActionBase : StyledElementAction
 
     [RequiresUnreferencedCode("This functionality is not compatible with trimming.")]
     public bool CanExecute() => Compare(Binding ?? GetParentBinding());
-
-        
+   
     [RequiresUnreferencedCode("This functionality is not compatible with trimming.")]
-    protected virtual bool Compare(object? binding) => Compare(binding, ComparisonCondition, Value);
+    protected virtual bool Compare(object? binding)
+    {
+        return ComparisonConditionTypeHelper.Compare(binding, ComparisonCondition, Value);
+    }
 
     protected object? GetParentBinding()
     {
@@ -137,101 +139,4 @@ public abstract class IfElseActionBase : StyledElementAction
     {
         return IfElseBehavior.ExecuteIfElseActions(Actions, sender, parameter);
     }
-
-    [RequiresUnreferencedCode("This functionality is not compatible with trimming.")]
-    public static bool Compare(object? leftOperand, ComparisonConditionType operatorType, object? rightOperand)
-    {
-        if (leftOperand is not null && rightOperand is not null)
-        {
-            Type leftType = leftOperand.GetType();
-            Type rightType = rightOperand.GetType();
-
-            if (leftType != rightType)
-            {
-                // cast via converter
-                var value = rightOperand.ToString();
-                if (value is not null)
-                {
-                    try
-                    {
-                        rightOperand = TypeConverterHelper.Convert(value, leftType);
-                        if (rightOperand is not null)
-                            rightType = rightOperand.GetType();
-                    }
-                    catch
-                    {
-                        // nothing
-                    }
-                }
-            }
-
-            // compare via IComparable
-            if (leftOperand is IComparable leftComparableOperand && rightOperand is IComparable rightComparableOperand)
-            {
-                if (leftType == rightType)
-                    return EvaluateComparable(leftComparableOperand, operatorType, rightComparableOperand);
-                else
-                    return ConvertAndEvaluateComparable(leftComparableOperand, operatorType, rightComparableOperand);
-            }
-        }
-
-        // compare links
-        switch (operatorType)
-        {
-            case ComparisonConditionType.Equal:
-                return Equals(leftOperand, rightOperand);
-
-            case ComparisonConditionType.NotEqual:
-                return !Equals(leftOperand, rightOperand);
-
-            default:
-                return false;
-            //throw new InvalidOperationException();
-        }
-    }
-
-    private static bool EvaluateComparable(IComparable leftOperand, ComparisonConditionType operatorType, IComparable rightOperand)
-    {
-        var comparison = leftOperand.CompareTo(rightOperand);
-        return operatorType switch
-        {
-            ComparisonConditionType.Equal => comparison == 0,
-            ComparisonConditionType.NotEqual => comparison != 0,
-            ComparisonConditionType.LessThan => comparison < 0,
-            ComparisonConditionType.LessThanOrEqual => comparison <= 0,
-            ComparisonConditionType.GreaterThan => comparison > 0,
-            ComparisonConditionType.GreaterThanOrEqual => comparison >= 0,
-            _ => false
-        };
-    }
-
-    private static bool ConvertAndEvaluateComparable(IComparable leftOperand, ComparisonConditionType operatorType, IComparable rightOperand)
-    {
-        object? convertedOperand = null;
-        try
-        {
-            convertedOperand = Convert.ChangeType(rightOperand, leftOperand.GetType(), CultureInfo.CurrentCulture);
-        }
-        catch (FormatException)
-        {
-            // FormatException: Convert.ChangeType("hello", typeof(double), ...);
-        }
-        catch (InvalidCastException)
-        {
-            // InvalidCastException: Convert.ChangeType(4.0d, typeof(Rectangle), ...);
-        }
-        catch
-        {
-            return false;
-        }
-
-        if (convertedOperand is null || convertedOperand is not IComparable comparableOperand)
-        {
-            return operatorType == ComparisonConditionType.NotEqual;
-        }
-
-        return EvaluateComparable(leftOperand, operatorType, comparableOperand);
-    }
-
-    
 }
