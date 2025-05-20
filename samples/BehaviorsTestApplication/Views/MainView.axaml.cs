d5a7af03dc4efc9b666857faa5ba359a3ebffce1
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-05-20 22:00:21 +0200

    Add search box for sample pages

--- samples/BehaviorsTestApplication/Views/MainView.axaml.cs
+++ samples/BehaviorsTestApplication/Views/MainView.axaml.cs
@@ -1,13 +1,41 @@
-﻿using Avalonia.Controls;
+using System;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace BehaviorsTestApplication.Views;
 
 public partial class MainView : UserControl
 {
+    private TextBox? _searchBox;
+    private TabControl? _tabControl;
+
     public MainView()
     {
         InitializeComponent();
+        _tabControl = this.FindControl<TabControl>("SamplesTabControl");
+        _searchBox = this.FindControl<TextBox>("SearchBox");
+        if (_searchBox != null)
+        {
+            _searchBox.GetObservable(TextBox.TextProperty)
+                .Subscribe(text => Search(text));
+        }
+    }
+
+    private void Search(string? text)
+    {
+        if (string.IsNullOrWhiteSpace(text) || _tabControl == null)
+            return;
+
+        var item = _tabControl.Items
+            .OfType<TabItem>()
+            .FirstOrDefault(i => i.Header is string header &&
+                header.Contains(text, StringComparison.OrdinalIgnoreCase));
+        if (item != null)
+        {
+            _tabControl.SelectedItem = item;
+        }
     }
 
     private void InitializeComponent()
