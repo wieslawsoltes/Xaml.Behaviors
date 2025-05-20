6ec06a43bda7c6e72c0dbc2b7552716b81b70d5d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-05-20 22:11:50 +0200

    Add search filtering to sample pages

--- samples/BehaviorsTestApplication/Views/MainView.axaml.cs
+++ samples/BehaviorsTestApplication/Views/MainView.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System.Linq;
 
 namespace BehaviorsTestApplication.Views;
 
@@ -8,6 +9,26 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void OnSearchTextChanged(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var query = SearchBox.Text?.ToLowerInvariant() ?? string.Empty;
+        var visibleCount = 0;
+
+        foreach (var item in PagesTabControl.Items.OfType<TabItem>())
+        {
+            var header = item.Header?.ToString()?.ToLowerInvariant() ?? string.Empty;
+            var visible = header.Contains(query);
+            item.IsVisible = visible;
+
+            if (visible)
+            {
+                visibleCount++;
+            }
+        }
+
+        NoMatchesText.IsVisible = visibleCount == 0;
     }
 
     private void InitializeComponent()
