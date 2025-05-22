44b0f0b9fe4af0b416ff7a46b4f256f3fd6d7c47
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-05-22 19:38:14 +0200

    Add FluidMoveBehavior for Avalonia and sample

---
+++ samples/BehaviorsTestApplication/ViewModels/FluidMoveBehaviorViewModel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace BehaviorsTestApplication.ViewModels;
+
+public class FluidMoveBehaviorViewModel : ViewModelBase
+{
+    public FluidMoveBehaviorViewModel()
+    {
+        for (var i = 1; i <= 8; i++)
+        {
+            Items.Add(i);
+        }
+
+        ShuffleCommand = ReactiveCommand.Create(Shuffle);
+    }
+
+    public ObservableCollection<int> Items { get; } = new();
+
+    public ICommand ShuffleCommand { get; }
+
+    private void Shuffle()
+    {
+        var rnd = new Random();
+        var array = Items.ToList();
+        Items.Clear();
+        foreach (var value in array.OrderBy(_ => rnd.Next()))
+        {
+            Items.Add(value);
+        }
+    }
+}
