24ead2f3535592f2d7c9570d73a3624dba39a19a
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-05-26 22:40:53 +0200

    Add AutoSelectBehavior and sample

---
+++ src/Xaml.Behaviors.Interactions.Custom/TextBox/AutoSelectBehavior.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Threading;
+
+namespace Avalonia.Xaml.Interactions.Custom;
+
+/// <summary>
+/// Automatically selects the entire content of the associated <see cref="TextBox"/> when it is loaded.
+/// </summary>
+public sealed class AutoSelectBehavior : StyledElementBehavior<TextBox>
+{
+    /// <inheritdoc/>
+    protected override void OnLoaded()
+    {
+        base.OnLoaded();
+
+        Dispatcher.UIThread.Post(() => AssociatedObject?.SelectAll());
+    }
+}
