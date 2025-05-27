bc3c9a639354b51fb79b5237e1f46383afd1335a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-05-27 19:40:39 +0200

    Make ActionsProperty a StyledProperty

--- src/Xaml.Behaviors.Interactivity/StyledElement/StyledElementTrigger.cs
+++ src/Xaml.Behaviors.Interactivity/StyledElement/StyledElementTrigger.cs
@@ -13,16 +13,26 @@ public abstract class StyledElementTrigger : StyledElementBehavior, ITrigger
     /// <summary>
     /// Identifies the <seealso cref="Actions"/> avalonia property.
     /// </summary>
-    public static readonly DirectProperty<StyledElementTrigger, ActionCollection> ActionsProperty =
-        AvaloniaProperty.RegisterDirect<StyledElementTrigger, ActionCollection>(nameof(Actions), t => t.Actions);
-
-    private ActionCollection? _actions;
+    public static readonly StyledProperty<ActionCollection> ActionsProperty =
+        AvaloniaProperty.Register<StyledElementTrigger, ActionCollection>(nameof(Actions));
 
     /// <summary>
     /// Gets the collection of actions associated with the behavior. This is an avalonia property.
     /// </summary>
     [Content]
-    public ActionCollection Actions => _actions ??= [];
+    public ActionCollection Actions
+    {
+        get => GetValue(ActionsProperty);
+        set => SetValue(ActionsProperty, value);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StyledElementTrigger"/> class.
+    /// </summary>
+    protected StyledElementTrigger()
+    {
+        SetCurrentValue(ActionsProperty, []);
+    }
 
     internal override void Initialize()
     {
