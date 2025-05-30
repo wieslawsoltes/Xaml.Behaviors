ff12783e75352ffc4b67c1c277b97f9f9d10eb71
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-05-30 13:46:55 +0200

    Add NumericUpDown validation sample

---
+++ src/Xaml.Behaviors.Interactions.Custom/Validation/MaxValueValidationRule.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Avalonia.Xaml.Interactions.Custom;
+
+/// <summary>
+/// Validation rule that checks that a numeric value is less than or equal to a maximum value.
+/// </summary>
+public class MaxValueValidationRule : IValidationRule<decimal?>
+{
+    /// <summary>
+    /// Gets or sets the maximum value.
+    /// </summary>
+    public decimal MaxValue { get; set; }
+
+    /// <inheritdoc />
+    public string? ErrorMessage { get; set; } = "Value is above maximum.";
+
+    /// <inheritdoc />
+    public bool Validate(decimal? value)
+    {
+        return value is decimal v && v <= MaxValue;
+    }
+}
---
+++ src/Xaml.Behaviors.Interactions.Custom/Validation/MinValueValidationRule.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Avalonia.Xaml.Interactions.Custom;
+
+/// <summary>
+/// Validation rule that checks that a numeric value is greater than or equal to a minimum value.
+/// </summary>
+public class MinValueValidationRule : IValidationRule<decimal?>
+{
+    /// <summary>
+    /// Gets or sets the minimum value.
+    /// </summary>
+    public decimal MinValue { get; set; }
+
+    /// <inheritdoc />
+    public string? ErrorMessage { get; set; } = "Value is below minimum.";
+
+    /// <inheritdoc />
+    public bool Validate(decimal? value)
+    {
+        return value is decimal v && v >= MinValue;
+    }
+}
