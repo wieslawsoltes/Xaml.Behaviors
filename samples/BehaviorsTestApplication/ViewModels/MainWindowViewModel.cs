6d07fe604c21d09922dbed7c6442a00a617b6068
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-05-30 16:44:16 +0200

    Add NotNullValidationRule sample page

--- samples/BehaviorsTestApplication/ViewModels/MainWindowViewModel.cs
+++ samples/BehaviorsTestApplication/ViewModels/MainWindowViewModel.cs
@@ -72,6 +72,7 @@ public partial class MainWindowViewModel : ViewModelBase
         MyString = "";
         ValidatedText = "";
         ValidatedNumber = 0m;
+        SelectedItem = null;
 
         IsLoading = true;
         Progress = 30;
@@ -131,6 +132,10 @@ public partial class MainWindowViewModel : ViewModelBase
     [Reactive] public partial decimal ValidatedNumber { get; set; }
 
     [Reactive] public partial bool IsNumberValid { get; set; }
+
+    [Reactive] public partial ItemViewModel? SelectedItem { get; set; }
+
+    [Reactive] public partial bool IsItemValid { get; set; }
 
     public IObservable<int> Values { get; }
 
