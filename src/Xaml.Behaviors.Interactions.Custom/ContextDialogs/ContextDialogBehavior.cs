3e3c6ad49a3dd24e72dafcbfa0c1dbc5f2ab35dc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-06-02 10:20:26 +0200

    Fixes

--- src/Xaml.Behaviors.Interactions.Custom/ContextDialogs/ContextDialogBehavior.cs
+++ src/Xaml.Behaviors.Interactions.Custom/ContextDialogs/ContextDialogBehavior.cs
@@ -1,6 +1,9 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Metadata;
 using Avalonia.Xaml.Interactivity;
 
 namespace Avalonia.Xaml.Interactions.Custom;
