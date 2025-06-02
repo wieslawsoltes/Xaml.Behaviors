9b3b0f2d8b023ff090c64337efda47e932c258e7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-06-02 09:54:38 +0200

    Add headers

--- src/Xaml.Behaviors.Interactions.ViewModel/SetViewModelPropertyOnLoadBehavior.cs
+++ src/Xaml.Behaviors.Interactions.ViewModel/SetViewModelPropertyOnLoadBehavior.cs
@@ -1,4 +1,5 @@
-using System;
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 using Avalonia.Controls;
 using Avalonia.Xaml.Interactivity;
 
--- src/Xaml.Behaviors.Interactions.ViewModel/ToggleViewModelBooleanAction.cs
+++ src/Xaml.Behaviors.Interactions.ViewModel/ToggleViewModelBooleanAction.cs
@@ -1,5 +1,5 @@
-using System;
-using System.Reflection;
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
 using Avalonia.Xaml.Interactivity;
 
 namespace Avalonia.Xaml.Interactions.ViewModel;
