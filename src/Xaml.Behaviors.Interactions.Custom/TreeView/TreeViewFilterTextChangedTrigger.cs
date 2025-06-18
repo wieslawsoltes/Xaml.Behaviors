38f3fc22eb8469b3168862f7f1e6b370f24415db
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-06-18 13:27:04 +0200

    Update TreeViewFilterTextChangedTrigger.cs

--- src/Xaml.Behaviors.Interactions.Custom/TreeView/TreeViewFilterTextChangedTrigger.cs
+++ src/Xaml.Behaviors.Interactions.Custom/TreeView/TreeViewFilterTextChangedTrigger.cs
@@ -1,9 +1,8 @@
 // Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.Xaml.Interactions.Events;
+using Avalonia.Xaml.Interactivity;
 
 namespace Avalonia.Xaml.Interactions.Custom;
 
