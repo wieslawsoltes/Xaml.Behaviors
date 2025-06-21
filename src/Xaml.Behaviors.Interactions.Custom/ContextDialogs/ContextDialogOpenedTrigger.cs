de7265802057950cc1d5cc28d0e36e852809de90
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-06-21 12:05:58 +0200

    Fix

--- src/Xaml.Behaviors.Interactions.Custom/ContextDialogs/ContextDialogOpenedTrigger.cs
+++ src/Xaml.Behaviors.Interactions.Custom/ContextDialogs/ContextDialogOpenedTrigger.cs
@@ -1,8 +1,7 @@
 // Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
-
 using System.Diagnostics.CodeAnalysis;
-using Avalonia.Xaml.Interactions.Core;
+using Avalonia.Xaml.Interactivity;
 
 namespace Avalonia.Xaml.Interactions.Custom;
 
@@ -10,7 +9,7 @@ namespace Avalonia.Xaml.Interactions.Custom;
 /// Trigger that listens for the <c>Opened</c> event of <see cref="ContextDialogBehavior"/>.
 /// </summary>
 [RequiresUnreferencedCode("This functionality is not compatible with trimming.")]
-public class ContextDialogOpenedTrigger : EventTriggerBehavior
+public class ContextDialogOpenedTrigger : EventTriggerBase
 {
     static ContextDialogOpenedTrigger()
     {
