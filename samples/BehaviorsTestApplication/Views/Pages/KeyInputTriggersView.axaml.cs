77a35a11c74b5a121e50eb925b51733af5b19f52
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-06-24 10:47:43 +0200

    Add missing InputElement trigger samples

---
+++ samples/BehaviorsTestApplication/Views/Pages/KeyInputTriggersView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views.Pages;
+
+public partial class KeyInputTriggersView : UserControl
+{
+    public KeyInputTriggersView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
