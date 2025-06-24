0b83e21b566208b6e2cdb3bb306f99abb4432fe7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-06-24 13:33:54 +0200

    Fix errors

--- samples/BehaviorsTestApplication/Behaviors/AttachedToLogicalTreeMessageBehavior.cs
+++ samples/BehaviorsTestApplication/Behaviors/AttachedToLogicalTreeMessageBehavior.cs
@@ -1,4 +1,5 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Xaml.Interactivity;
 using Avalonia.Xaml.Interactions.Custom;
@@ -7,8 +8,15 @@ namespace BehaviorsTestApplication.Behaviors;
 
 public class AttachedToLogicalTreeMessageBehavior : AttachedToLogicalTreeBehavior<StyledElement>
 {
+    public static readonly StyledProperty<TextBlock?> TargetProperty =
+        AvaloniaProperty.Register<AttachedToLogicalTreeMessageBehavior, TextBlock?>(nameof(Target));
+
     [ResolveByName]
-    public TextBlock? Target { get; set; }
+    public TextBlock? Target
+    {
+        get => GetValue(TargetProperty);
+        set => SetValue(TargetProperty, value);
+    }
 
     protected override IDisposable OnAttachedToLogicalTreeOverride()
     {
