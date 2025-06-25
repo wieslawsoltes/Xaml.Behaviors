7edc3ac5523314e1f49e597f582ba8fbed129d23
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-06-25 10:30:30 +0200

    Add headless DataTriggerBehavior tests

--- tests/Xaml.Behaviors.Interactions.UnitTests/Core/DataTriggerBehaviorTests.cs
+++ tests/Xaml.Behaviors.Interactions.UnitTests/Core/DataTriggerBehaviorTests.cs
@@ -30,4 +30,42 @@ public class DataTriggerBehaviorTests
         Assert.Equal("75", window.TargetTextBox.Text);
         Assert.Equal(75d, window.TargetSlider.Value);
     }
+
+    [AvaloniaFact]
+    public void DataTriggerBehavior_002()
+    {
+        var window = new DataTriggerBehavior002();
+
+        window.Show();
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_002_0.png");
+
+        Assert.False(window.TargetBorder.IsVisible);
+        Assert.Equal(string.Empty, window.TargetTextBlock.Text);
+
+        window.InputTextBox.Text = "Hello";
+
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_002_1.png");
+
+        Assert.True(window.TargetBorder.IsVisible);
+        Assert.Equal("Hello", window.TargetTextBlock.Text);
+    }
+
+    [AvaloniaFact]
+    public void DataTriggerBehavior_003()
+    {
+        var window = new DataTriggerBehavior003();
+
+        window.Show();
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_003_0.png");
+
+        Assert.Equal("Less than 25", window.TargetTextBlock.Text);
+        Assert.Equal(0d, window.TargetSlider.Value);
+
+        window.TargetSlider.Value = 30d;
+
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_003_1.png");
+
+        Assert.Equal("Greater or equal 25", window.TargetTextBlock.Text);
+        Assert.Equal(30d, window.TargetSlider.Value);
+    }
 }
