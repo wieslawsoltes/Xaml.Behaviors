6ec912e362968fd21082414a2705c70d05bd981c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-06-25 10:53:59 +0200

    Add additional DataTriggerBehavior headless tests

---
+++ tests/Xaml.Behaviors.Interactions.UnitTests/Core/DataTriggerBehavior002.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Xaml.Interactions.UnitTests.Core;
+
+public partial class DataTriggerBehavior002 : Window
+{
+    public DataTriggerBehavior002()
+    {
+        InitializeComponent();
+    }
+}
--- tests/Xaml.Behaviors.Interactions.UnitTests/Core/DataTriggerBehaviorTests.cs
+++ tests/Xaml.Behaviors.Interactions.UnitTests/Core/DataTriggerBehaviorTests.cs
@@ -30,4 +30,44 @@ public class DataTriggerBehaviorTests
         Assert.Equal("75", window.TargetTextBox.Text);
         Assert.Equal(75d, window.TargetSlider.Value);
     }
+
+    [AvaloniaFact]
+    public void DataTriggerBehavior_002()
+    {
+        var window = new DataTriggerBehavior002();
+
+        window.Show();
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_002_0.png");
+
+        Assert.Equal("Unchecked", window.TargetTextBlock.Text);
+        Assert.False(window.TargetCheckBox.IsChecked);
+
+        window.Click(window.TargetCheckBox);
+
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_002_1.png");
+
+        Assert.Equal("Checked", window.TargetTextBlock.Text);
+        Assert.True(window.TargetCheckBox.IsChecked);
+    }
+
+    [AvaloniaFact]
+    public void DataTriggerBehavior_003()
+    {
+        var window = new DataTriggerBehavior003();
+
+        window.Show();
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_003_0.png");
+
+        Assert.Equal("Less than 50", window.TargetTextBlock.Text);
+        Assert.Equal(0d, window.TargetSlider.Value);
+
+        window.TargetSlider.Focus();
+        window.KeyPressQwerty(PhysicalKey.ArrowRight, RawInputModifiers.None);
+        window.KeyPressQwerty(PhysicalKey.ArrowRight, RawInputModifiers.None);
+
+        window.CaptureRenderedFrame()?.Save("DataTriggerBehavior_003_1.png");
+
+        Assert.Equal("50 or more", window.TargetTextBlock.Text);
+        Assert.Equal(50d, window.TargetSlider.Value);
+    }
 }
