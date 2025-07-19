7027e224199675da9540940fa33a694a6a8b9ab4
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-07-19 09:39:20 +0200

    Add drag-and-drop interaction tests

---
+++ tests/Xaml.Behaviors.Interactions.UnitTests/DragAndDrop/PanelDragDrop001.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Xaml.Interactions.UnitTests.DragAndDrop;
+
+public partial class PanelDragDrop001 : Window
+{
+    public PanelDragDrop001()
+    {
+        InitializeComponent();
+    }
+}
