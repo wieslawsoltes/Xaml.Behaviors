029a0ad025e75a749b5cfa93821b3ac945b9a552
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-09-30 09:32:46 +0200

    Add composition animations

---
+++ samples/BehaviorsTestApplication/Views/Pages/RotateAnimationView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BehaviorsTestApplication.Views.Pages;
+
+public partial class RotateAnimationView : UserControl
+{
+    public RotateAnimationView()
+    {
+        InitializeComponent();
+    }
+}
