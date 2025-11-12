aaf2f2d1f8fb37caf9298ac318c1832980aedb93
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-11-12 22:59:48 +0100

    Add BehaviorsExtensionMembersView

---
+++ samples/BehaviorsTestApplication/Views/Pages/BehaviorsExtensionMembersView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BehaviorsTestApplication.Views.Pages;
+
+public partial class BehaviorsExtensionMembersView : UserControl
+{
+    public BehaviorsExtensionMembersView()
+    {
+        InitializeComponent();
+    }
+}
