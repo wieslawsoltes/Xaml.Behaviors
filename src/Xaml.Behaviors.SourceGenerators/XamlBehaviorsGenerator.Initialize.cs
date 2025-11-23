4ec802f77f31b7424b0533764036fcac6ed40033
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-11-23 12:26:10 +0100

    Add copyright headers

--- src/Xaml.Behaviors.SourceGenerators/XamlBehaviorsGenerator.Initialize.cs
+++ src/Xaml.Behaviors.SourceGenerators/XamlBehaviorsGenerator.Initialize.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
 using Microsoft.CodeAnalysis;
 
 namespace Xaml.Behaviors.SourceGenerators
